a59ce1e996d81a20669c2f01b97e8bf86d7bebb8
AmrNabil-AN <amr12nabel@gmail.com>
2023-03-17 09:46:50 -0700

    add

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: print alphapet expet e and q
+ *
+ * Return: always 0 (success)
+ */
+
+	int main(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		if (x == 'e' || x == 'q')
+		       ++x
+		putchar(x);
+	}
+
+	putchar('\n');
+
+	return (0);
+}	
