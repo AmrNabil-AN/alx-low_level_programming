0cf920c24294ae666d6e1a845304beb9f4417e1c
AmrNabil-AN <amr12nabel@gmail.com>
2023-03-17 12:09:05 -0700

    add

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * descrption: print namber from 01 to 89
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int x = 0;
+	int y;
+
+	while (x <= 9 )
+	{
+		y = 0;
+		while (y <= 9)
+		{
+			if (x != y && x < y)
+			{
+				putchar(x + 48);
+				putchar(y + 48);
+				
+				if (x + y != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++y;
+		}
+		++x;
+	}
+	putchar('\n');
+
+	return (0);
+}
