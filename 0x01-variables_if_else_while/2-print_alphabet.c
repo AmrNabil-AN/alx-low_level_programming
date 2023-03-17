859e922c6d1ee6b05eed5c5ccfcc362ec673ac6b
AmrNabil-AN <amr12nabel@gmail.com>
2023-03-17 09:03:34 -0700

    while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * descrption: prints alphabet in lower case
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+	int main(void)
+{
+	char x= a;
+
+	while (x <= z)
+	{
+		putchar(x);
+		x++
+	}
+	putchar("\n");
+	return (0);
+}
