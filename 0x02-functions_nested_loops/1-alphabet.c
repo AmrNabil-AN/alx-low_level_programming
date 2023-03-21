c0209b42e699fd2645499616102136760616886f
AmrNabil-AN <amr12nabel@gmail.com>
2023-03-21 07:13:15 -0700

    aadd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,17 +7,14 @@
  *
  * Return: always 0 (success)
  */
-void print_alphabet(void);
-
-int main(void)
-{
-	void print_alphabet()
+void print_alphabet(void)
 {
 		char x;
 
 		for (x = 'a'; x <= 'z'; x++)
+
 		_putchar('x');
 		_putchar('\n');
-}
-	return (0);
+
+		return (0);
 }
