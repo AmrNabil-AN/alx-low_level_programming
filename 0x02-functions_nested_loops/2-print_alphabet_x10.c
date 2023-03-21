948f3b4ffcd3cf4c06b51900c5f24379c658f776
AmrNabil-AN <amr12nabel@gmail.com>
2023-03-21 12:45:47 -0700

    add

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print from a to z 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+	int y;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+
+	for (y = 0 ; y <= 0; y++)
+		_putchar('\n');
+}
+
+
