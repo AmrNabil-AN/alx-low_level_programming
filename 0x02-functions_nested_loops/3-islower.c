81a2674658b046949124f5cf20a01a700cda1e5c
AmrNabil-AN <amr12nabel@gmail.com>
2023-03-22 01:47:35 -0700

    add

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _islower(int c) - cheack if character lower cace or not
+ *
+ * return: always 0 (success)
+ */
+int _islower(int c);
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
