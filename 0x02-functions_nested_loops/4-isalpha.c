9053ccb245b0bef4880cd149d0b6b5dd534addd3
AmrNabil-AN <amr12nabel@gmail.com>
2023-03-22 02:11:12 -0700

    add

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if the letter lower case or upper case
+ * @c: charcter
+ *
+ * Return: 1 if the lower or upper case found
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
