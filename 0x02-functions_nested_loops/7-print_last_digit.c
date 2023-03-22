f00f82e1248f1b9c4486b8b63ab8d7a4b9c16858
AmrNabil-AN <amr12nabel@gmail.com>
2023-03-22 02:59:52 -0700

    add

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the code 
+ * @n:character
+ *
+ * Return: the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+	_putchar('\n');
+	return (last_digit);
+}
+
