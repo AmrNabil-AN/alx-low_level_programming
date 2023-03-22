e340d1d11f4750933a607953f0e31bbbae15226e
AmrNabil-AN <amr12nabel@gmail.com>
2023-03-22 02:48:08 -0700

    add

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abc - check the absolute value
+ * @n: character
+ *
+ * Return: value
+ */
+int _abc(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else 
+	{
+		return (n)
+	}
+}
