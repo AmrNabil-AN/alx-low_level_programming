40f09ff565accbdf12926adb76dae98dd1c9c1c4
AmrNabil-AN <amr12nabel@gmail.com>
2023-03-22 03:18:33 -0700

    add

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,14 +9,13 @@
 
 int print_last_digit(int n)
 {
-	int last_digit = (n % 10);
+	int last_digit = (n % 2);
 
 	if (last_digit < 0)
 	{
 		last_digit *= -1;
 
 	_putchar(last_digit + '0');
-	_putchar('\n');
 	}
 
 	return (last_digit);
