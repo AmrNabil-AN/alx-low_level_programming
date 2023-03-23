ae8af5b8d1816915fc986d5650f3c8fd3bff8c74
AmrNabil-AN <amr12nabel@gmail.com>
2023-03-23 06:04:16 -0700

    add

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check multiplies
+ * @a: character
+ * @b: character
+ *
+ * Return: multiblays
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
