da6a9d90d0fda5ef8dd89b7178ab0c7797e6d7ed
AmrNabil-AN <amr12nabel@gmail.com>
2023-03-23 01:54:17 -0700

    add

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * maint - test function that prints if intger is + or -
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i=98;
+	positive_ornegative(i);
+
+	return (0);
+}
