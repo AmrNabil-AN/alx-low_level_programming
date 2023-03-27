3afd921b9f6075ec5f10769677aedd73e7f2febe
AmrNabil-AN <amr12nabel@gmail.com>
2023-03-27 02:55:37 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - make pointer valuo 98
+ * @n: pointer
+ *
+ * Return: 0 (success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+fndef MAIN_H
+#define MAIN_H
+
+int _putchar(int c);
+void reset_to_98(int *n);
+
+
+
+
+
+
+
+#endif
