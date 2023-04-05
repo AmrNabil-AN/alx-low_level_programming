8975519b35e8180ba85cdedb3dce682451c45440
AmrNabil-AN <amr12nabel@gmail.com>
2023-04-05 03:13:00 -0700

    add

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * factorial - return factorial number
+ * @n: intger
+ * Return: always 0 (success)
+ */
+
+int factorial(int n);
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+		return (n * factorial(n - 1);
+}
